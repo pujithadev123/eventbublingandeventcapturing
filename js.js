5c25f289bcbccf9c7a25fb95ccef54c0f79a8453
G.V.S.Pujitha <pujithagvs@gmail.com>
2024-11-23 20:32:38 +0530

    uncomments are added

--- js.js
+++ js.js
@@ -4,95 +4,95 @@ let Child=document.getElementById("child")
 
 
 
-// Gparent.addEventListener('click',(e)=>{
+Gparent.addEventListener('click',(e)=>{
  
-//     console.log("grand parent clicked")
-// })
+    console.log("grand parent clicked")
+})
 
-// Parent.addEventListener('click',(e)=>{
+Parent.addEventListener('click',(e)=>{
    
-//     console.log("parent clicked")
-// })
+    console.log("parent clicked")
+})
 
-// Child.addEventListener('click',(e)=>{
+Child.addEventListener('click',(e)=>{
     
-//     console.log("child clicked")
-// })
+    console.log("child clicked")
+})
 
 
-// Gparent.addEventListener('click',(e)=>{
-//     e.stopPropagation()
-//     console.log("grand parent clicked")
-//     Gparent.style.backgroundColor="red"
-// })
+Gparent.addEventListener('click',(e)=>{
+    e.stopPropagation()
+    console.log("grand parent clicked")
+    Gparent.style.backgroundColor="red"
+})
 
-// Parent.addEventListener('click',(e)=>{
-//     e.stopPropagation()
-//     console.log("parent clicked")
-//     Parent.style.backgroundColor="pink"
-// })
-
-
-// Child.addEventListener('click',(e)=>{
-//     e.stopPropagation()
-//     console.log("child clicked")
-//     Child.style.backgroundColor="blue"
-// })
+Parent.addEventListener('click',(e)=>{
+    e.stopPropagation()
+    console.log("parent clicked")
+    Parent.style.backgroundColor="pink"
+})
 
 
-// Gparent.addEventListener('click',(e)=>{
-//     e.stopPropagation()
-//     setTimeout(()=>{
-//         console.log("Grand parent clicked")
-//         Gparent.style.backgroundColor="red"
-//         },3000)
+Child.addEventListener('click',(e)=>{
+    e.stopPropagation()
+    console.log("child clicked")
+    Child.style.backgroundColor="blue"
+})
+
+
+Gparent.addEventListener('click',(e)=>{
+    e.stopPropagation()
+    setTimeout(()=>{
+        console.log("Grand parent clicked")
+        Gparent.style.backgroundColor="red"
+        },3000)
    
-// })
+})
 
-// Parent.addEventListener('click',(e)=>{
-//     e.stopPropagation()
-//     setTimeout(()=>{
-//         console.log("parent clicked")
-//         Parent.style.backgroundColor="pink"
-//         },2000)
-// })
+Parent.addEventListener('click',(e)=>{
+    e.stopPropagation()
+    setTimeout(()=>{
+        console.log("parent clicked")
+        Parent.style.backgroundColor="pink"
+        },2000)
+})
 
 
-// Child.addEventListener('click',(e)=>{
-//     e.stopPropagation()
-//     setTimeout(()=>{
-//     console.log("child clicked")
-//     Child.style.backgroundColor="blue"
-//     },1000)
+Child.addEventListener('click',(e)=>{
+    e.stopPropagation()
+    setTimeout(()=>{
+    console.log("child clicked")
+    Child.style.backgroundColor="blue"
+    },1000)
     
-// })
+})
 
-// Gparent.addEventListener('click',(e)=>{
-//     // e.stopPropagation()
-//     setTimeout(()=>{
-//         console.log("Grand parent clicked")
-//         Gparent.style.backgroundColor="red"
-//         },3000)
+Gparent.addEventListener('click',(e)=>{
+    // e.stopPropagation()
+    setTimeout(()=>{
+        console.log("Grand parent clicked")
+        Gparent.style.backgroundColor="red"
+        },3000)
    
-// },true)
+},true)
 
-// Parent.addEventListener('click',(e)=>{
-//     // e.stopPropagation()
-//     setTimeout(()=>{
-//         console.log("parent clicked")
-//         Parent.style.backgroundColor="pink"
-//         },2000)
-// },true)
+Parent.addEventListener('click',(e)=>{
+    // e.stopPropagation()
+    setTimeout(()=>{
+        console.log("parent clicked")
+        Parent.style.backgroundColor="pink"
+        },2000)
+},true)
 
 
-// Child.addEventListener('click',(e)=>{
-//     // e.stopPropagation()
-//     setTimeout(()=>{
-//     console.log("child clicked")
-//     Child.style.backgroundColor="blue"
-//     },1000)
+Child.addEventListener('click',(e)=>{
+    // e.stopPropagation()
+    setTimeout(()=>{
+    console.log("child clicked")
+    Child.style.backgroundColor="blue"
+    },1000)
     
-// },true)
+},true)
 
 
 Gparent.addEventListener("click",(e)=>{
